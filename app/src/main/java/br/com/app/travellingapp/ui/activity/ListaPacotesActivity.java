6675f09a5c6cdfda6885e395679393d6939732bd
jonathanmperes <emailmarceloperes@gmail.com>
2022-04-28 18:47:07 -0300

    Initial commit

---
+++ app/src/main/java/br/com/app/travellingapp/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,16 @@
+package br.com.app.travellingapp.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.app.travellingapp.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
